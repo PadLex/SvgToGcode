f75ee187e93c635a727c476c82d8f616bbe9cf4a
Padlex <alexpadula2001@gmail.com>
2021-01-31 11:52:13 +0100

    Add setup.py for publishing on pypi

---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as file:
+    readme = file.read()
+
+setuptools.setup(
+    name="svg_to_gcode",
+    version="1.0.0",
+    author="Padlex",
+    author_email="",
+    description="The definitive NPM module to construct gcode from svg files.",
+    long_description=readme,
+    long_description_content_type="text/markdown",
+    url="https://github.com/PadLex/SvgToGcode",
+    packages=setuptools.find_packages(exclude=("testing", "testing.*")),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU Lesser General Public License v3 (LGPLv3)",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
