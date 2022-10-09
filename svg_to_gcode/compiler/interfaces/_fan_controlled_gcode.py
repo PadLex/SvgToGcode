90adbd20d9242adaf62aecbdeb6bd052a641ae2b
johannesnoordanus <52376919+johannesnoordanus@users.noreply.github.com>
2022-10-09 22:03:44 +0200

    Override the set_laser_power method, use gcode M7 instead of M8
     Override the set_laser_power method, use gcode M7 - mist coolant - instead of M8 - flood coolant .
    (note that setting 'fan' must be True for this to work)

--- svg_to_gcode/compiler/interfaces/_fan_controlled_gcode.py
+++ svg_to_gcode/compiler/interfaces/_fan_controlled_gcode.py
@@ -4,18 +4,17 @@ from svg_to_gcode import formulas
 
 class FanControlledGcode(Gcode):
 
-    def laser_off(self):
-        if self._current_power is None or self._current_power > 0:
-            self._current_power = 0
-            return f"M107;"
-
-        return ''
-
+    # Override the set_laser_power method, use gcode M7 - mist coolant - instead of M8 - flood coolant -
+    # (note that setting 'fan' must be True for this to work)
     def set_laser_power(self, power):
-        self._current_power = power
-
         if power < 0 or power > 1:
             raise ValueError(f"{power} is out of bounds. Laser power must be given between 0 and 1. "
                              f"The interface will scale it correctly.")
+	# set power for next linear move
+        self._next_laser_power = int(formulas.linear_map(self._machine_params['minimum_laser_power'], self._machine_params['maximum_laser_power'], power))
 
-        return f"M106 S{formulas.linear_map(0, 255, power)};"
+        # (fan on when available, use M7 instead of M8), laser_on
+        new_mode = ("\nM7" if self._machine_params['fan'] else '') + ("\n" + self._laser_mode if self._laser_mode_changed else '')
+        self._laser_mode_changed = False
+	# return laser mode (M3 constant laser power or M4 dynamic laser power) when laser mode changed
+        return f"; Cut at {self._next_speed} {self._unit}/min, {int(power * 100)}% power{new_mode}"
