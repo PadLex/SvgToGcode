92d78c83af515d54dd7c66428faa24a6408daea2
johannesnoordanus <52376919+johannesnoordanus@users.noreply.github.com>
2022-10-09 16:21:20 +0200

    Update for new interface

--- testing/comparison_tests/basic_usage/test.py
+++ testing/comparison_tests/basic_usage/test.py
@@ -4,7 +4,7 @@ from svg_to_gcode.compiler import Compiler, interfaces
 
 def run_test(svg_string):
 
-    gcode_compiler = Compiler(interfaces.Gcode, 1000, 300, 2)
+    gcode_compiler = Compiler(interfaces.Gcode, params={"laser_power":1,"movement_speed":300,"maximum_laser_power":255,"pass_depth":2})
 
     curves = parse_string(svg_string, transform_origin=True)
     gcode_compiler.append_curves(curves)
